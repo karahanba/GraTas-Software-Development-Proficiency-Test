3ba61ea2d2a58bcd902bf4cefc03c4d299e8ccc6
karahanba <batuhankarahan@hotmail.com>
2021-05-24 16:47:26 +0300

    Updated output & added comments

--- Gratas3/Program.cs
+++ Gratas3/Program.cs
@@ -20,161 +20,98 @@ namespace Gratas3
             Console.WriteLine("listenin boyutunu verin...");
             int n = int.Parse(Console.ReadLine());
 
-            List<double> numberlist = new List<double>();           // all randomly generated numbers are listed here
             double num;                                             // will be assigned to a random value between 0.0 and 1.0
+
+            List<double> numberlist = new List<double>();           // all randomly generated numbers are listed here
             List<double> roundlist = new List<double>();            // to find and update item in decimallist using the same index value 
             List<double> decimallist = new List<double>();          // list of repeated decimals with same whole-number part
             List<double> largestFractions = new List<double>();     // list of values to show as output
             List<double> roundForPairs = new List<double>();        // to find and update item in largestFraction using the same index value 
-            int matchcount = 0;
+            //int matchcount = 0;                                   // self explanatory
 
             for (int i = 0; i < n; i++)
             {
-                double sayi = random.NextDouble();
+                double numRatio = random.NextDouble();
 
-                num = 10 * sayi;
+                num = 10 * numRatio;
 
                 numberlist.Add(num);
                 
-                if (roundlist.Contains(Math.Floor(numberlist[i]))) //bu if içine düşen her sayı tekrar etmiştir.dolayısıyla büyük olanı decimal liste yazılmalı
+                if (roundlist.Contains(Math.Floor(numberlist[i]))) 
                 {
-                    matchcount++;
-                    Console.WriteLine("Toplamda "+matchcount+" eşleşme var");
-                    //int index = i;
-                    //Console.WriteLine(numberlist.IndexOf(Math.Floor(numberlist[i])));
+                    //optional feature for counting matched items
+                    //matchcount++;
+                    //Console.WriteLine("Tekrarlananların sayısı: " + matchcount);
+                    
                     int matchindex = roundlist.IndexOf(Math.Floor(numberlist[i]));
 
                     if (decimallist[matchindex]<numberlist[i])
                     {
-                        decimallist[matchindex] = numberlist[i];/// burada olması ne kadar doğru? incelenmeli...//bu değer largestFraction'a yazılmalı..
-                        if (!roundForPairs.Contains(Math.Floor(decimallist[matchindex])))//!largestFractions.Contains(decimallist[matchindex]))
+                        decimallist[matchindex] = numberlist[i]; // numberlist[i], the bigger value, assigned as new decimallist[matchindex]
+                        if (!roundForPairs.Contains(Math.Floor(decimallist[matchindex])))
                         {
-                            if (!largestFractions.Contains(decimallist[matchindex]))//sorun burada!!!!
+                            if (!largestFractions.Contains(decimallist[matchindex]))
                             {
-                                largestFractions.Add(decimallist[matchindex]); // burada tekrardan ekleme hatalı olabilir !!!
+                                largestFractions.Add(decimallist[matchindex]); 
                                 roundForPairs.Add(Math.Floor(decimallist[matchindex])); // this will help to eliminate recurrence of the same whole-number part in largestFractions
                             }
-
-
-                            //if (!roundForPairs.Contains(Math.Floor(decimallist[matchindex])))
-                            //{
-                            //    largestFractions.Add(decimallist[matchindex]);
-                            //    roundForPairs.Add(Math.Round(decimallist[matchindex])); // this will help to eliminate recurrence of the same whole-number part in largestFractions
-                            //}
-                            //else
-                            //{
-                            //    //smaller value with the same whole-number part is overwrited by decimallist[matchindex]
-                            //    largestFractions[roundForPairs.IndexOf(Math.Floor(decimallist[matchindex]))] = decimallist[matchindex];
-                            //}
-                            
                         }
-                        else // roundforPairs contains floor decimallist(index)
+
+                        else // if roundforPairs contains that item, then fractional item in largestFractions list must be updated
                         {
-
-                            //largestFractions[largestFractions.IndexOf(largestFractions.FirstOrDefault(check => check.Equals(roundlist[matchindex])))] = decimallist[matchindex];
-                            //largestFractions[largestFractions.IndexOf(largestFractions.Where(x => Math.Floor(x.CompareTo(roundlist[matchindex]))))] = decimallist[matchindex];
-
                             //smaller value with the same whole-number part is overwrited by decimallist[matchindex]
                             largestFractions[roundForPairs.IndexOf(Math.Floor(decimallist[matchindex]))] = decimallist[matchindex];
-                            
-                            
-                            //yeni gelen random sayı eldekinden büyük olduğundalargestFraction daki ilgili index güncellenmeli..OK
                         }
                         
                     }
-                    else//gelen sayı eldekinden küçükse, eldeki büyük olan alınmalı
+                    else    // decimallist[matchindex]>numberlist[i] for this case
                     {
-                        if (!largestFractions.Contains(decimallist[matchindex]))//eldeki sayı listeye eklenmemişse ekle
+                        if (!largestFractions.Contains(decimallist[matchindex]))    // Add item item if non-existent 
                         {
                             largestFractions.Add(decimallist[matchindex]);
                             roundForPairs.Add(Math.Floor(decimallist[matchindex])); // this will help to eliminate recurrence of the same whole-number part in largestFractions
                         }
-                        
-
-                        //if (!roundForPairs.Contains(Math.Floor(decimallist[matchindex])))//!largestFractions.Contains(decimallist[matchindex]))
-                        //{
-                        //    //Do nothing?
-
-                        //    //roundForPairs.Add(Math.Floor(decimallist[matchindex]));
-
-                      
-
-                        //}
-                        //else//içeriyor
-                        //{
-                            
-                        //    if (!largestFractions.Contains(numberlist[i]))//decimallist[matchindex]))
-                        //    {
-                        //        largestFractions.Add(decimallist[matchindex]);
-                        //        roundForPairs.Add(Math.Round(decimallist[matchindex])); // this will help to eliminate recurrence of the same whole-number part in largestFractions
-                        //    }
-
-
-                        //    //if (!roundForPairs.Contains(Math.Floor(decimallist[matchindex])))
-                        //    //{
-                        //    //    largestFractions.Add(decimallist[matchindex]);
-                        //    //    roundForPairs.Add(Math.Round(decimallist[matchindex])); // this will help to eliminate recurrence of the same whole-number part in largestFractions
-                        //    //}
-                        //}
                     }
 
                 }
-                else
+                else  //  No match => add values to roundlist and decimallist
                 {
-                    
-                    Console.WriteLine("eşleşme yok, roundlist ve decimallist'e değerler eklendi.");
                     roundlist.Add(Math.Floor(numberlist[i]));
                     decimallist.Add(numberlist[i]);
-                    
-
                 }
-
-                //Console.WriteLine(numberlist[i]);
-                //Console.WriteLine(esassayi);
             }
 
 
-            Console.WriteLine(matchcount + "toplam eşleşme");
-            Console.WriteLine("/////LargestFractions/////");
+            //Console.WriteLine("Toplam eşleşme: "+matchcount);
+            Console.WriteLine("/////    LargestFractions    /////");
             foreach (var item in largestFractions)
             {
-
-                Console.WriteLine(item);
-
+                Console.WriteLine(Math.Round(item,2));
             }
-            Console.WriteLine("/////numberlist/////");
+            
+            //Console.WriteLine("/////    All generated numbers in numberlist     /////");
+            //foreach (var item in numberlist)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-            foreach (var item in numberlist)
-            {
+            //Console.WriteLine("/////     decimallist     /////");
+            //foreach (var item in decimallist)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-                Console.WriteLine(item);
+            //Console.WriteLine("/////      roundlist       /////");
+            //foreach (var item in roundlist)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-            }
-
-            Console.WriteLine("/////decimallist/////");
-            foreach (var item in decimallist)
-            {
-
-                Console.WriteLine(item);
-
-            }
-            Console.WriteLine("/////roundlist/////");
-            foreach (var item in roundlist)
-            {
-
-                Console.WriteLine(item);
-
-            }
-            Console.WriteLine("/////roundForPairs/////");
-            foreach (var item in roundForPairs)
-            {
-
-                Console.WriteLine(item);
-
-            }
-
-
-
+            //Console.WriteLine("/////      roundForPairs      /////");
+            //foreach (var item in roundForPairs)
+            //{
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
